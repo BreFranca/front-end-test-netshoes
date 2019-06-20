54737517220e5f770cce28f3e9a68df889d5f865
Breno Franca <bre.sfranca@gmail.com>
2019-06-20 11:46:24 -0300

    Updates

--- precache-manifest.dd71cf38ef9e3988e1676470b3d8c866.js
+++ precache-manifest.dd71cf38ef9e3988e1676470b3d8c866.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d5ff5dda3daace47e4467ce467b8081c",
+    "revision": "487d4996c021b4c0fa941904ae40e84c",
     "url": "https://brefranca.github.io/front-end-test-netshoes/index.html"
   },
   {
-    "revision": "15accf6d156522095d7b",
+    "revision": "272eac2f2e648d524fd1",
     "url": "https://brefranca.github.io/front-end-test-netshoes/static/css/main.f24d1afb.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://brefranca.github.io/front-end-test-netshoes/static/js/2.b386d207.chunk.js"
   },
   {
-    "revision": "15accf6d156522095d7b",
-    "url": "https://brefranca.github.io/front-end-test-netshoes/static/js/main.b82e4106.chunk.js"
+    "revision": "272eac2f2e648d524fd1",
+    "url": "https://brefranca.github.io/front-end-test-netshoes/static/js/main.ce68fcdd.chunk.js"
   },
   {
     "revision": "4234cd25d9eb68749d17",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://brefranca.github.io/front-end-test-netshoes/precache-manifest.b690d52f9fbb7c817aaa8abce9ef6264.js"
+  "https://brefranca.github.io/front-end-test-netshoes/precache-manifest.dd71cf38ef9e3988e1676470b3d8c866.js"
 );
 
 self.addEventListener('message', (event) => {
